e050e60f29356283f5fbc669aa9e653e81def434
eunbiviakwon <eunbiviakwon@gmail.com>
2019-12-19 17:09:50 +0900

    OneToOne default

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'one_to_one.apps.OneToOneConfig',
     'many_to_many.apps.ManyToManyConfig',
     'myapp.apps.MyappConfig',
     'many_to_one.apps.ManyToOneConfig',
