ad9676cb91641f1e602be013d9cc2a846b76c8ea
eunbiviakwon <eunbiviakwon@gmail.com>
2019-12-19 13:50:45 +0900

    ManyToOne 세팅

--- app/myapp/models.py
+++ app/myapp/models.py
@@ -8,6 +8,8 @@ class Person(models.Model):
         ('L', 'Large'),
     )
 
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    shirt_size = models.CharField(
+        '셔츠 사이즈',
+        max_length=1, choices=SHIRT_SIZES, help_text='서츠 사이즈 이다')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
